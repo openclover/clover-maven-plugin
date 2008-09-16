0d01bc0346b0293725e5ee884d894582b8916449
Nick Pellow <npellow@atlassian.com>
2008-09-16 08:47:25 +0000

    CLMVN-73: adding test project for test optimization

---
+++ src/it/optmized/src/test/java/com/atlassian/maven/plugin/clover/samples/simple/SlowTest.java
@@ -0,0 +1,14 @@
+package com.atlassian.maven.plugin.clover.samples.simple;
+
+import junit.framework.TestCase;
+
+/**
+ */
+public class SlowTest extends TestCase {
+
+    public void testWaitFor3Seconds() throws InterruptedException {
+        Thread.sleep(3000);
+        // just trying to get this to recompile
+    }
+
+}
