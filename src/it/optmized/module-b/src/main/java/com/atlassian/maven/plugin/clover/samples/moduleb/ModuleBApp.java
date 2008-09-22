bdce0fc5e84f6e0ddd91af8e163ba83530fd6796
Nick Pellow <npellow@atlassian.com>
2008-09-22 07:30:10 +0000

    CLOV-73: moved into module

---
+++ src/it/optmized/module-b/src/main/java/com/atlassian/maven/plugin/clover/samples/moduleb/ModuleBApp.java
@@ -0,0 +1,22 @@
+package com.atlassian.maven.plugin.clover.samples.moduleb;
+
+public class ModuleBApp {
+	
+	public static ModuleBApp getApp() {
+		return new ModuleBApp();
+	}
+
+	public String getName() {
+		return "ModuleBApp";
+	}
+	
+	public int getNumber() {
+		
+		int num = 0;
+		num += 2;
+		num *= 2;
+		num /= 2;
+		num -= 2;
+		return num;
+	}
+}
---
+++ src/it/optmized/module-b/src/test/java/com/atlassian/maven/plugin/clover/samples/moduleb/ModuleBAppTest.java
@@ -0,0 +1,15 @@
+package com.atlassian.maven.plugin.clover.samples.moduleb;
+
+import junit.framework.TestCase;
+
+public class ModuleBAppTest extends TestCase {
+	
+	public void testGetName() {
+		assertEquals(ModuleBApp.getApp().getName(), "ModuleBApp");
+	}
+	
+	
+	public void testGetNumber() {
+		assertEquals(ModuleBApp.getApp().getNumber(), 0);
+	}
+}
