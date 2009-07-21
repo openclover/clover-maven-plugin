8b54cb1007788bd7419ad4bb3f99a3111e10602c
Nick Pellow <npellow@atlassian.com>
2009-07-21 04:22:31 +0000

    CLOV-641: added CrossModuleTest.java to track down problem test opt. was having in conf

--- src/it/optmized/module-b/src/test/java/com/atlassian/maven/plugin/clover/samples/moduleb/CrossModuleTest.java
+++ src/it/optmized/module-b/src/test/java/com/atlassian/maven/plugin/clover/samples/moduleb/CrossModuleTest.java
@@ -2,6 +2,7 @@ package com.atlassian.maven.plugin.clover.samples.moduleb;
 
 import junit.framework.TestCase;
 import com.atlassian.maven.plugin.clover.samples.modulea.Simple;
+import com.atlassian.maven.plugin.clover.samples.modulea.Dummy;
 
 /**
  * This test, tests the class in module-a
@@ -11,6 +12,12 @@ public class CrossModuleTest extends TestCase {
     public void testModuleA() {
         Simple simple = new Simple();
         simple.someMethod();
+        assertNotNull(simple);
+    }
+
+    public void testModuleAAgain() {
+        Dummy simple = new Dummy();
+        simple.dummyMethod();
         assertNotNull(simple);
     }
 }
