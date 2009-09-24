b70b7ad87cd790048131b4458b410429bfdb3c30
Nick Pellow <npellow@atlassian.com>
2009-09-24 05:21:19 +0000

    CLMVN-127: committing patch from Sumit Shah. New Mojo that forks the lifecycle, but only runs to the 'test' phase, not the 'install' phase as clover2:instrumnet does.

---
+++ src/main/java/com/atlassian/maven/plugin/clover/CloverInstrumentTestMojo.java
@@ -0,0 +1,29 @@
+package com.atlassian.maven.plugin.clover;
+
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * This goal behaves exactly like the instrument goal, however when forking the lifecycle - it runs only to the 'test'
+ * phase instead of all the way to the 'install' phase.
+ *
+ * This goal should be used as an optimization - ie. if the phases after 'test' take a very long time to run.
+ *
+ * Instrument all sources using Clover and forks a custom lifecycle to execute project's tests on the instrumented
+ * code so that a Clover database is created.
+ *
+ * @goal instrument-test
+ * @execute phase="test" lifecycle="clover"
+ *
+ */
+public class CloverInstrumentTestMojo extends CloverInstrumentMojo
+{
+    /**
+     * {@inheritDoc}
+     * @see com.atlassian.maven.plugin.clover.internal.AbstractCloverMojo#execute()
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+    }
+}
