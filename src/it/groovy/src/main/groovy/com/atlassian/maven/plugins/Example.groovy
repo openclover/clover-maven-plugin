9329bd033c1367ddb6fbbab305385a8cb6268203
Nick Pellow <npellow@atlassian.com>
2010-02-23 06:37:22 +0000

    CLOV-890: adding func test for the groovy

---
+++ src/it/groovy/src/main/groovy/com/atlassian/maven/plugins/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.atlassian.maven.plugins
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
