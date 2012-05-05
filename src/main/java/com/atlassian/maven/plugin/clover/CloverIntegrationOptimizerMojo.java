dd40c75e7bc266b255a9c53ddb892eea3e7d12fc
Michael Studman <mstudman@atlassian.com>
2012-05-05 20:32:58 +0000

    CLMVN-148: Cchange for optimizing integration tests

--- src/main/java/com/atlassian/maven/plugin/clover/CloverIntegrationOptimizerMojo.java
+++ src/main/java/com/atlassian/maven/plugin/clover/CloverIntegrationOptimizerMojo.java
@@ -1,5 +1,11 @@
 package com.atlassian.maven.plugin.clover;
 
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+
+import java.util.List;
+
 /**
  * Sets the 'test' property on the project which is used by the maven-surefire-plugin to determine which tests are run.
  * If a snapshot file from a previous build, is found, that will be used to determine what tests should be run.
@@ -8,4 +14,22 @@ package com.atlassian.maven.plugin.clover;
  * @phase pre-integration-test
  */
 public class CloverIntegrationOptimizerMojo extends CloverOptimizerMojo {
+
+    protected List extractNestedStrings(String elementName, Plugin surefirePlugin) {
+        List value = null;
+
+        //Try to get the value for the Surefire config for the "integration-test", if there is one
+        //but default to the global config otherwise
+        List executions = surefirePlugin.getExecutions();
+        for (int i = 0; i < executions.size(); i++) {
+            PluginExecution execution = (PluginExecution) executions.get(i);
+            if ("integration-test".equals(execution.getPhase())) {
+                Xpp3Dom config = (Xpp3Dom) execution.getConfiguration();
+                value = config == null ? null : extractNestedStrings(elementName, config);
+                break;
+            }
+        }
+
+        return value == null ? super.extractNestedStrings(elementName, surefirePlugin) : null;
+    }
 }
