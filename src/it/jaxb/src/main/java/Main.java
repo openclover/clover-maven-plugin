c1797e4488d493c0065107f6fd0e71cf0c71620a
Marek Parfianowicz <mparfianowicz@atlassian.com>
2013-03-06 10:55:42 +0100

    NONE: added sample source file as clover does not add target/src-instrumented to list of build directories if src/main/java is empty

---
+++ src/it/jaxb/src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+import com.sun.enterprise.config.serverbeans.AccessLog;
+
+public class Main {
+    public static void main(String args[]) {
+        AccessLog accessLog = new AccessLog();
+    }
+}
