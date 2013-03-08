587c0b0fb18a2ebe4c49b694179a9130f60f79bf
Marek Parfianowicz <mparfianowicz@atlassian.com>
2013-03-08 16:07:34 +0100

    CLOV-1166: integration tests to check clover-for-maven against different groovy-eclipse-plugin / groovy-eclipse-batch / groovy-all versions

---
+++ src/it/groovy-eclipse-plugin/src/main/java/com/atlassian/maven/plugins/Helper.java
@@ -0,0 +1,10 @@
+package com.atlassian.maven.plugins;
+
+/**
+ * Example Java class.
+ */
+public class Helper {
+    public void help(final Example example) {
+        example.show();
+    }
+}    
