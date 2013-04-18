face34bf0538a410e5e6a0a264f6d753c6d7d862
Marek Parfianowicz <mparfianowicz@atlassian.com>
2013-04-18 13:42:55 +0200

    CLOV-1150: handling a case for the <sourceDirectory> and <testSourceDirectory> options in pom.xml has flipped-down my redesign from CLOV-1144 :-)) new changes: 1) AbstactInstrumenter.redirectSourceDirectories() has new condition for replacing original source directory - it does not redirect to the instrumented one if the original is src/main/groovy (or src/test/groovy) - it handles a case when we have a pure groovy project with sourceDirectory=src/main/groovy (and testSourceDirectory=src/test/groovy) 2) GroovyMainSourceScanner and GroovyTestSourceScanner was integrated in MainSourceScanner and TestSourceScanner 3) GroovyMain/TestSourceScanner.removeOwnSourceRoots()  was replaced by Main/TestSourceScanner.removeGroovySourceRoot() 4) filtering of src/main|test/groovy folder was put directly into CloverSourceScanner.getSourceFilesToInstrument() with a skipGroovySourceDirectory=true parameter
    plus minor corrections like generics, @Nullable, @NotNull annotations, foreach loops etc.

--- src/main/java/com/atlassian/maven/plugin/clover/internal/instrumentation/AbstractInstrumenter.java
+++ src/main/java/com/atlassian/maven/plugin/clover/internal/instrumentation/AbstractInstrumenter.java
@@ -19,11 +19,10 @@ package com.atlassian.maven.plugin.clover.internal.instrumentation;
  * under the License.
  */
 
+import com.atlassian.clover.spi.lang.Language;
 import com.atlassian.maven.plugin.clover.MvnLogger;
 import com.atlassian.maven.plugin.clover.internal.CompilerConfiguration;
 import com.atlassian.maven.plugin.clover.internal.scanner.CloverSourceScanner;
-import com.atlassian.maven.plugin.clover.internal.scanner.GroovyMainSourceScanner;
-import com.atlassian.maven.plugin.clover.internal.scanner.GroovyTestSourceScanner;
 import com.atlassian.maven.plugin.clover.internal.scanner.LanguageFileExtensionFilter;
 import com.cenqua.clover.CloverInstr;
 import com.cenqua.clover.Logger;
@@ -33,7 +32,6 @@ import org.codehaus.plexus.util.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -64,9 +62,8 @@ public abstract class AbstractInstrumenter {
      */
     public void instrument() throws MojoExecutionException {
         final CloverSourceScanner scanner = getSourceScanner();
-
         // get source files to be instrumented, but only for Java as they will be instrumented by CloverInstr
-        final Map javaFilesToInstrument = scanner.getSourceFilesToInstrument(LanguageFileExtensionFilter.JAVA_LANGUAGE);
+        final Map<String, String[]> javaFilesToInstrument = scanner.getSourceFilesToInstrument(LanguageFileExtensionFilter.JAVA_LANGUAGE, true);
         if (javaFilesToInstrument.isEmpty()) {
             getConfiguration().getLog().info("No Clover instrumentation done on source files in: "
                     + getCompileSourceRoots() + " as no matching sources files found (JAVA_LANGUAGE)");
@@ -74,15 +71,20 @@ public abstract class AbstractInstrumenter {
             instrumentSources(javaFilesToInstrument, outputSourceDirectory);
         }
 
-        // in case when 'src/main/java' (or 'src/test/java') contains *.groovy source files (this is a trick possible
+        // find groovy files in all compilation roots and copy them
+        //
+        // 1) in case when 'src/main/java' (or 'src/test/java') contains *.groovy source files (this is a trick possible
         // with a groovy-eclipse-plugin, see http://groovy.codehaus.org/Groovy-Eclipse+compiler+plugin+for+Maven
         // "Setting up source folders / Do nothing") we must copy *.groovy files as well
         // reason: 'src/main/java' (or 'src/test/java') will be redirected to 'target/clover/src-instrumented'
         // (or 'target/clover/src-test-instrumented') and Groovy compiler must be able to find these groovy sources
-        final Map/*<String,String[]>*/ groovyFilesToInstrument = scanner.getSourceFilesToInstrument(LanguageFileExtensionFilter.GROOVY_LANGUAGE);
-        // don't copy files from 'src/main/groovy' and 'src/test/groovy' because these source roots are not being redirected
-        GroovyMainSourceScanner.removeOwnSourceRoots(groovyFilesToInstrument.keySet());
-        GroovyTestSourceScanner.removeOwnSourceRoots(groovyFilesToInstrument.keySet());
+        //
+        // 2) however we shall not copy groovy files from 'src/(main|test)/groovy' because these source roots are not
+        // being redirected to 'target/clover/src-(test-)instrumented'; furthermore groovy-eclipse-plugin has
+        // 'src/(main|test)/groovy' location hardcoded, so copying files would end up with 'duplicate class' build error
+        final Map<String, String[]> groovyFilesToInstrument = scanner.getSourceFilesToInstrument(LanguageFileExtensionFilter.GROOVY_LANGUAGE, true);
+
+        // copy groovy files
         if (!groovyFilesToInstrument.isEmpty()) {
             copyExcludedFiles(groovyFilesToInstrument, outputSourceDirectory);
         }
@@ -91,10 +93,8 @@ public abstract class AbstractInstrumenter {
         // thus won't be compiled by the compile plugin. This will lead to compilation errors if any other
         // file depends on any of these excluded files.
         if (configuration.copyExcludedFiles()) {
-            final Map/*<String,String[]>*/ explicitlyExcludedFiles = scanner.getExcludedFiles();
-            // avoid having the same excluded file in two locations (in 'src/xxx/groovy' and 'src-xxx-instrumented')
-            GroovyMainSourceScanner.removeOwnSourceRoots(explicitlyExcludedFiles.keySet());
-            GroovyTestSourceScanner.removeOwnSourceRoots(explicitlyExcludedFiles.keySet());
+            final Map<String, String[]> explicitlyExcludedFiles = scanner.getExcludedFiles();
+            // 'src/(main|test)/groovy' is already filtered-out in getExcludedFiles()
             copyExcludedFiles(explicitlyExcludedFiles, outputSourceDirectory);
         }
     }
@@ -124,20 +124,26 @@ public abstract class AbstractInstrumenter {
 
         // Maven2 limitation: changing the source directory doesn't change the compile source roots
         // See http://jira.codehaus.org/browse/MNG-1945
-        List<String> sourceRoots = new ArrayList<String>(getCompileSourceRoots());
+        final List<String> sourceRoots = new ArrayList<String>(getCompileSourceRoots());
 
         // Clean all source roots to add them again in order to keep the same original order of source roots.
         getCompileSourceRoots().removeAll(sourceRoots);
 
+        final CloverSourceScanner scanner = getSourceScanner();
         for (final String sourceRoot : sourceRoots) {
             if (new File(oldSourceDirectory).exists() && sourceRoot.equals(oldSourceDirectory)) {
-                // add redirected location (e.g. 'src/main/java' -> 'target/clover/src-instrumented') instead of the
-                // original source root
-                addCompileSourceRoot(getSourceDirectory());
-            }
-
-            else if ( !(getConfiguration().includesAllSourceRoots() && isGeneratedSourcesDirectory(sourceRoot)) )
-            {
+                // if compilation root is the same as original source directory:
+                // a) if it's a Java directory then use location of instrumented sources instead of the original source
+                // root (e.g. 'src/main/java' -> 'target/clover/src-instrumented')
+                // b) if it's a Groovy directory then don't change the location because we don't instrument Groovy on
+                // a source level, so the Clover's instrumented folder is empty; Groovy files will be instrumented
+                // during compilation on the AST level (e.g. 'src/main/groovy' -> 'src/main/groovy')
+                if (scanner.isSourceRootForLanguage(sourceRoot, Language.Builtin.GROOVY))  {
+                    addCompileSourceRoot(sourceRoot);
+                } else {
+                    addCompileSourceRoot(getSourceDirectory());
+                }
+            } else if ( !(getConfiguration().includesAllSourceRoots() && isGeneratedSourcesDirectory(sourceRoot)) ) {
                 // if includeAllSourceRoots=true then ignore the original generated sources directory (e.g. target/generated/xyz),
                 // because Clover will instrument them and store instrumented version (e.g. target/clover/src-instrumented);
                 // compiler should know only the latter location, otherwise we would end up with the same classes included twice
@@ -285,7 +291,7 @@ public abstract class AbstractInstrumenter {
             }
         }
 
-        return parameters.toArray(new String[0]);
+        return parameters.toArray(new String[parameters.size()]);
     }
 
     private void addCustomContexts(final List<String> parameters, final Set<Map.Entry<String, String>> contexts, final String flag) {
--- src/main/java/com/atlassian/maven/plugin/clover/internal/scanner/MainSourceScanner.java
+++ src/main/java/com/atlassian/maven/plugin/clover/internal/scanner/MainSourceScanner.java
@@ -19,20 +19,55 @@ package com.atlassian.maven.plugin.clover.internal.scanner;
  * under the License.
  */
 
+import com.atlassian.clover.spi.lang.Language;
 import com.atlassian.maven.plugin.clover.internal.CompilerConfiguration;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Computes the list of main source files to instrument.
  */
 public class MainSourceScanner extends AbstractSourceScanner {
+    @NotNull
+    public static final String SRC_MAIN_JAVA = "src" + File.separator + "main" + File.separator + "java";
+
+    @NotNull
+    public static final String SRC_MAIN_GROOVY = "src" + File.separator + "main" + File.separator + "groovy";
+
     public MainSourceScanner(final CompilerConfiguration configuration, final String outputSourceDirectory) {
         super(configuration, outputSourceDirectory);
     }
 
-    protected List<String> getSourceRoots() {
-        return getConfiguration().getProject().getCompileSourceRoots();
+    /**
+     * From a list of provided <code>sourceRoots</code> remove SRC_MAIN_GROOVY root
+     *
+     * @param sourceRoots    list of source roots from
+     * @see #SRC_MAIN_GROOVY
+     * @see #getSourceFilesToInstrument()
+     */
+    @Override
+    public void removeGroovySourceRoot(@NotNull final Set<String> sourceRoots) {
+        removeSourceRoot(sourceRoots, SRC_MAIN_GROOVY);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isSourceRootForLanguage(String sourceRoot, Language language) {
+        return (language.getName().equals(Language.Builtin.JAVA.getName()) && sourceRoot.endsWith(SRC_MAIN_JAVA))
+                || (language.getName().equals(Language.Builtin.GROOVY.getName()) && sourceRoot.endsWith(SRC_MAIN_GROOVY));
+    }
+
+    protected List<String> getCompileSourceRoots() {
+        final List<String> roots = new ArrayList<String>(getConfiguration().getProject().getCompileSourceRoots());
+        // add hardcoded SRC_MAIN_GROOVY (clover2:setup might be called before the groovy-eclipse-plugin adds it
+        // as a compilation root)
+        roots.add(SRC_MAIN_GROOVY);
+        return roots;
     }
 
     protected String getSourceDirectory() {
