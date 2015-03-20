6826d07b5c3e523ea734a0d3e5653e180a762786
Marek Parfianowicz <mparfianowicz@atlassian.com>
2015-03-20 12:53:10 +0100

    CLOV-1632: repository pollution protection, initial version; created shouldRedirectArtifacts() and shouldRedirectOutputDirectories() which are used in redirectArtifact() and redirectOutputDirectories() respectively; thanks to this, empty implementations of these two methods were removed from CloverSetupMojo; created repositoryPollutionProtection parameter (can be used in clover2:setup/instr/instr-test)

--- src/main/java/com/atlassian/maven/plugin/clover/CloverInstrumentMojo.java
+++ src/main/java/com/atlassian/maven/plugin/clover/CloverInstrumentMojo.java
@@ -21,12 +21,11 @@ package com.atlassian.maven.plugin.clover;
 
 import com.atlassian.maven.plugin.clover.internal.AbstractCloverInstrumentMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import com.atlassian.maven.plugin.clover.internal.AbstractCloverMojo;
 
 /**
  * Instrument all sources using Clover and forks a custom lifecycle to execute project's tests on the instrumented
  * code so that a Clover database is created.
- *
+ * <p/>
  * <p>Note: We're forking a lifecycle because we don't want the Clover instrumentation to affect the main lifecycle
  * build. This will prevent instrumented sources to be put in production by error. Thus running
  * <code>mvn install</code> on a project where this <code>instrument</code> goal has been specified will run the
@@ -35,16 +34,25 @@ import com.atlassian.maven.plugin.clover.internal.AbstractCloverMojo;
  *
  * @goal instrument
  * @execute phase="install" lifecycle="clover"
- *
  */
-public class CloverInstrumentMojo extends AbstractCloverInstrumentMojo
-{
+public class CloverInstrumentMojo extends AbstractCloverInstrumentMojo {
+
     /**
      * {@inheritDoc}
+     *
      * @see com.atlassian.maven.plugin.clover.internal.AbstractCloverMojo#execute()
      */
-    public void execute()
-        throws MojoExecutionException
-    {
+    public void execute() throws MojoExecutionException {
+        super.execute();
+    }
+
+    @Override
+    protected boolean shouldRedirectArtifacts() {
+        return true;
+    }
+
+    @Override
+    protected boolean shouldRedirectOutputDirectories() {
+        return true;
     }
 }
