9e8cbe3cd7e55f7796eaa7ff6549f19c03fea481
Marek Parfianowicz <mparfianowicz@atlassian.com>
2015-03-23 12:57:21 +0100

    CLOV-1632: integration test to verify various combinations of clover2:setup/instrument + goals/phases + clean/default/site life cycle

---
+++ src/it/pollutionProtection/src/main/java/A.java
@@ -0,0 +1,5 @@
+public class A {
+    public boolean a() {
+        return true;
+    }
+}
---
+++ src/it/pollutionProtection/src/test/java/ATest.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ATest {
+    @Test
+    public void testA() {
+        Assert.assertTrue(new A().a());
+    }
+}
