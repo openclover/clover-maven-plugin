a751983d314de3e3f376e6c350a48c9bff11953c
Marek Parfianowicz <mparfianowicz@atlassian.com>
2019-04-27 16:57:03 +0200

    OC-84: add <testSources> configuration option for clover:setup / clover:instr

---
+++ src/main/java/com/atlassian/maven/plugin/clover/internal/configuration/AbstractJavaEntity.java
@@ -0,0 +1,40 @@
+package com.atlassian.maven.plugin.clover.internal.configuration;
+
+/**
+ * Something which has a name, annotations and javadocs. Such as a class or a method.
+ */
+public class AbstractJavaEntity {
+
+    /** A regex on which to match the name. Optional. */
+    private String name;
+
+    /** A regex on which to match the annotation. Optional. */
+    private String annotation;
+
+    /** A regex on which to match the javadoc tags. Optional. */
+    private String tag;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAnnotation() {
+        return annotation;
+    }
+
+    public void setAnnotation(String annotation) {
+        this.annotation = annotation;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+}
