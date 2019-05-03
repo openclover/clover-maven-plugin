5385092a31f91c5fcf55d9b17ea16d7212cf4050
Marek Parfianowicz <mparfianowicz@atlassian.com>
2019-05-04 01:33:54 +0200

    OC-84: added ExcludedOne.setUp method to execute all our test-alike and non-test methods to verify how Clover records code coverage for them; completed verifications.xml for them

--- src/it/custom-test-pattern/src/test/java/com/atlassian/maven/plugin/clover/samples/testsources/excluded/ExcludedOne.java
+++ src/it/custom-test-pattern/src/test/java/com/atlassian/maven/plugin/clover/samples/testsources/excluded/ExcludedOne.java
@@ -1,6 +1,12 @@
 package com.atlassian.maven.plugin.clover.samples.testsources.excluded;
 
+import com.atlassian.maven.plugin.clover.samples.testsources.ExampleOne;
+import com.atlassian.maven.plugin.clover.samples.testsources.ExampleTwoIT;
+import com.atlassian.maven.plugin.clover.samples.testsources.NoTestSuiteSoNotOne;
+import com.atlassian.maven.plugin.clover.samples.testsources.NotExampleThree;
 import com.atlassian.maven.plugin.clover.samples.testsources.TestSuite;
+import com.atlassian.maven.plugin.clover.samples.testsources.classic.ExampleThree;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -11,6 +17,20 @@ import static org.junit.Assert.assertTrue;
  */
 @TestSuite
 public class ExcludedOne {
+
+    @Before
+    public void setUp() {
+        // execute some code via surefire to generate coverage and see if a tests were recorded or not
+        new ExampleOne().testExampleOne();
+        new ExampleOne().notTestExampleOne();
+        new ExampleTwoIT().getRequest();
+        new ExampleTwoIT().postRequest();
+        new ExampleTwoIT().deleteRequest();
+        new NoTestSuiteSoNotOne().thisIsNotATestMethod();
+        new NotExampleThree().testNotExampleThree();
+        new ExampleThree().testExampleThree();
+        new ExampleThree().testNotExampleThree();
+    }
 
     @Test
     public void testSomeMethod() {
